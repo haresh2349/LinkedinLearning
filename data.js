f3ba738732414518615c32b5a99954f70f94ee46
haresh2349 <hareshcsolanki07@gmail.com>
2022-03-29 14:22:26 +0530

    browse update

---
+++ data.js
@@ -0,0 +1,336 @@
+let bussinessData = [
+  {
+    head: "Business Analysis and Strategy",
+    list: [
+      "Data Analysis",
+      "Business Strategy",
+      "Business Intelligence",
+      "Data Visualization",
+      "Operations Management",
+    ],
+  },
+  {
+    head: "Business Software and Tools",
+    list: [
+      "Data Analysis",
+      "Data Visualization",
+      "Spreadsheets",
+      "Computer Skills",
+      "Personal Productivity Software",
+    ],
+  },
+  {
+    head: "Career Development",
+    list: ["Career Management", "Job Searching", "Personal Branding"],
+  },
+  {
+    head: "Customer Service",
+    list: [
+      "CRM Software",
+      "Customer Service Skills",
+      "Customer Service Management",
+      "Contact Centers",
+      "Service Metrics",
+    ],
+  },
+  {
+    head: "Finance and Accounting",
+    list: [
+      "Small Business Finance",
+      "Corporate Finance",
+      "Accounting Skills",
+      "Personal Finance",
+      "Accounting Software",
+    ],
+  },
+  {
+    head: "Human Resources",
+    list: [
+      "LinkedIn Talent Insights Certifications",
+      "Talent Management",
+      "Diversity and Inclusion",
+      "HR Strategy",
+      "Hiring and Interviewing",
+    ],
+  },
+  {
+    head: "Leadership and Management",
+    list: [
+      "Communication",
+      "Leadership Skills",
+      "Business Strategy",
+      "Teams and Collaboration",
+      "Talent Management",
+    ],
+  },
+  {
+    head: "Marketing",
+    list: [
+      "LinkedIn Marketing Certifications",
+      "Meta Certifications",
+      "Social Media Marketing",
+      "Advertising and Promotion",
+      "Small Business Marketing",
+    ],
+  },
+  {
+    head: "Professional Development",
+    list: [
+      "Well-Being and Self-Care",
+      "Communication",
+      "Personal Effectiveness",
+      "Teams and Collaboration",
+      "Diversity and Inclusion",
+    ],
+  },
+  {
+    head: "Project Management",
+    list: [
+      "Operations Management",
+      "Project Management Skills",
+      "Project Management Software",
+      "Business Analysis",
+      "Agile Project Management",
+    ],
+  },
+  {
+    head: "Sales",
+    list: [
+      "CRM Software",
+      "Sales Skills",
+      "Social Selling",
+      "Sales Management",
+      "Sales Metrics",
+    ],
+  },
+  {
+    head: "Small Business and Entrepreneurship",
+    list: [
+      "Small Business Marketing",
+      "Small Business Managemen",
+      "Small Business Finance",
+      "Entrepreneurship",
+      "Design Business",
+    ],
+  },
+  {
+    head: "Training and Education",
+    list: [
+      "E-Learning Software",
+      "Instructional Design",
+      "Teaching",
+      "Educational Technology",
+      "Corporate Training",
+    ],
+  },
+];
+let creativeData = [
+  {
+    head: "AEC",
+    list: [
+      "Rendering",
+      "3D Modeling",
+      "2D Drafting And Drawing",
+      "Real-Time",
+      "BIM",
+    ],
+  },
+  {
+    head: "Animation and Illustration",
+    list: [
+      "Rendering",
+      "3D Modeling",
+      "3D Animation",
+      "Drawing",
+      "Illustration",
+    ],
+  },
+  {
+    head: "Audio and Music",
+    list: [
+      "Digital Audio Workstations",
+      "Music Production",
+      "Audio Engineering",
+      "Sound Design",
+      "Audio Recording",
+    ],
+  },
+  {
+    head: "Graphic Design",
+    list: [
+      "Asset Management",
+      "Drawing",
+      "Typography",
+      "Digital Publishing",
+      "Design Thinking",
+    ],
+  },
+  {
+    head: "Motion Graphics and VFX",
+    list: [
+      "Rendering",
+      "Motion Graphics",
+      "Typography",
+      "Compositing",
+      "Visual Effects",
+    ],
+  },
+  {
+    head: "Photography",
+    list: [
+      "Image Editing",
+      "Asset Management",
+      "Analog Photography",
+      "Photo Compositing",
+      "Portrait Photography",
+    ],
+  },
+  {
+    head: "Product and Manufacturing",
+    list: [
+      "Rendering",
+      "3D Modeling",
+      "2D Drafting And Drawing",
+      "Real-Time",
+      "Product and Industrial Design",
+    ],
+  },
+  {
+    head: "User Experience",
+    list: [
+      "Design Thinking",
+      "UX Design",
+      "Content Strategy",
+      "Interaction Design",
+      "User Interface Prototyping",
+    ],
+  },
+  {
+    head: "Video",
+    list: [
+      "Video Editing",
+      "Video Post-Production",
+      "Shooting Video",
+      "Video Production",
+      "Video Color Grading",
+    ],
+  },
+  {
+    head: "Visualization and Real-Time",
+    list: [
+      "Rendering",
+      "3D Modeling",
+      "2D Drafting And Drawing",
+      "Real-Time",
+      "Architectural Visualization",
+    ],
+  },
+  {
+    head: "Web Design",
+    list: [
+      "Web Standards",
+      "Interactive Web Content",
+      "Web Graphics",
+      "Responsive Web Design",
+      "Mobile Web Design",
+    ],
+  },
+];
+let techData = [
+  {
+    head: "Cloud Computing",
+    list: [
+      "Cloud Platforms",
+      "Cloud Services",
+      "Cloud Foundations",
+      "Cloud Development",
+      "Cloud Administration",
+    ],
+  },
+  {
+    head: "Data Science",
+    list: [
+      "Data Analysis",
+      "Business Intelligence",
+      "Data Visualization",
+      "Big Data",
+      "Machine Learning",
+    ],
+  },
+  {
+    head: "Database Management",
+    list: [
+      "Database Development",
+      "Database Administration",
+      "Data Resource Management",
+      "Data Centers",
+    ],
+  },
+  {
+    head: "DevOps",
+    list: ["DevOps Tools", "DevOps Foundations", "Agile Software Development"],
+  },
+  {
+    head: "IT Help Desk",
+    list: [
+      "Client Operating Systems",
+      "Help Desk Skills",
+      "Operating System Distribution",
+      "Upgrade and Maintenance",
+      "Software Support",
+    ],
+  },
+  {
+    head: "Mobile Development",
+    list: [
+      "Cross-Platform Development",
+      "iOS Development",
+      "Android Development",
+      "Mobile Game Development",
+    ],
+  },
+  {
+    head: "Network and System Administration",
+    list: [
+      "Network Administration",
+      "Server Administration",
+      "Virtualization",
+      "Internet of Things",
+      "Enterprise Content Management",
+    ],
+  },
+  {
+    head: "Security",
+    list: [
+      "Network Security",
+      "Security Management and Policy",
+      "Vulnerability Management",
+      "Identity and Access Management",
+      "Security Awareness",
+    ],
+  },
+  {
+    head: "Software Development",
+    list: [
+      "Programming Languages",
+      "Software Development Tools",
+      "Database Development",
+      "Game Development",
+      "Programming Foundations",
+    ],
+  },
+  {
+    head: "Web Development",
+    list: [
+      "Front-End Web Development",
+      "Back-End Web Development",
+      "Web Development Tools",
+      "Full-Stack Web Development",
+      "Content Management Systems (CMS)",
+    ],
+  },
+];
+
+localStorage.setItem("bussinessData", JSON.stringify(bussinessData));
+localStorage.setItem("creativeData", JSON.stringify(creativeData));
+localStorage.setItem("techData", JSON.stringify(techData));
