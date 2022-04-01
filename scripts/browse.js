666f5e176887be2455800dcdd78665fe4d384243
haresh2349 <hareshcsolanki07@gmail.com>
2022-04-02 00:57:47 +0530

    updatedslider

--- scripts/browse.js
+++ scripts/browse.js
@@ -1,12 +1,2 @@
-let slidDiv = document.querySelectorAll("#slidCont > div");
-let div = document.querySelector("#slidCont > div:nth-child(1)");
-console.log(div, "div");
-console.log(slidDiv, "hover");
-let desc = document.querySelector(".desc");
-let durr = document.querySelector(".durr");
-slidDiv.forEach((el) => {
-  el.addEventListener("mouseover", () => {
-    desc.classList.toggle("hover");
-    durr.classList.toggle("hover");
-  });
-});
+let div = document.querySelectorAll(".slidBox");
+console.log(div);
